b3e75468bca2314d9c8581c2c552bf4f9d46638c
furkankarakocmobilist <furkan.karakoc@mobilistbilisim.com>
2023-01-14 14:46:31 +0300

    create GestureRecognizerApp

--- GestureRecognizerApp/ViewController.swift
+++ GestureRecognizerApp/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var imageLabel: UILabel!
+    
+    var isMickey = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imageView.isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(changePic))
+        imageView.addGestureRecognizer(gestureRecognizer)
+        
     }
-
-
+    @objc func changePic() {
+     if isMickey == true {
+            imageView.image = UIImage(named: "minnie")
+            imageLabel.text = "Minnie"
+            isMickey = false
+      } else {
+            imageView.image = UIImage(named: "mickey")
+            imageLabel.text = "Mickey"
+            isMickey = true
+        }
+    }
 }
 
